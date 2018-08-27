60a009d885e077962fed8298dec8d8c4df602d4e
Dheeraj Reddy Pailla <dheerajpreddy@gmail.com>
2018-08-27 11:23:02 +0530

    Finished question a

--- answer_a.py
+++ answer_a.py
@@ -4,32 +4,33 @@
 from tokens import *
 
 # Import required packages
-from collections import Counter
 import matplotlib.pyplot as plt
 
-data = twitter.search(q='@iiit_hyderabad',result_type='mixed',count = 100)
+queries = ['@iiit_hyderabad', '@iiscbangalore']
+for query in queries:
+    from collections import Counter
+    data = twitter.search(q=query,result_type='mixed',count = 100)
 
-statuses = data['statuses']
-concat_str = "";
-for post in statuses:
-    concat_str = concat_str + " " + post['text']
+    statuses = data['statuses']
+    concat_str = "";
+    for post in statuses:
+        concat_str = concat_str + " " + post['text']
 
-words = concat_str.split()
-Counter = Counter(words)
-most_frequent = Counter.most_common(20)
-x = []
-y = []
-for i in range(0, len(most_frequent)):
-    x.append(most_frequent[i][0])
-    y.append(most_frequent[i][1])
-    # print most_frequent[0][0]
-print x
-print y
+    words = concat_str.split()
+    Counter = Counter(words)
+    most_frequent = Counter.most_common(20)
+    x = []
+    y = []
+    for i in range(0, len(most_frequent)):
+        x.append(most_frequent[i][0])
+        y.append(most_frequent[i][1])
+        # print most_frequent[0][0]
+    print x
+    print y
 
 
-plt.figure(figsize=(20, 5))  # width:20, height:5
-plt.bar(x, y, align='center', width=0.3)
-plt.xlabel('Words')
-plt.ylabel('Frequency')
-plt.show()
-# plt.savefig('testplot.png')
+    plt.figure(figsize=(20, 5))  # width:20, height:5
+    plt.bar(x, y, align='center', width=0.3)
+    plt.xlabel('Words')
+    plt.ylabel('Frequency')
+    plt.savefig(query + '.png')
