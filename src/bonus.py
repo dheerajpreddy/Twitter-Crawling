e0a0d45902fabdb61a790733ef90b09cb5404920
Dheeraj Reddy Pailla <dheerajpreddy@gmail.com>
2018-08-30 21:03:58 +0530

    Finished bonus question

--- src/bonus.py
+++ src/bonus.py
@@ -2,17 +2,26 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from time import sleep
+from bs4 import BeautifulSoup
 
-driver = webdriver.Chrome('/Applications/Google Chrome.app')
-username = "iiit_hyderabad"
+options = webdriver.ChromeOptions()
+options.binary_location = '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
+options.add_argument('window-size=800x841')
+options.add_argument('headless')
+
+driver = webdriver.Chrome(chrome_options=options)
+username = "ponguru"
 driver.get("https://twitter.com/" + username)
-# assert "Python" in driver.title
-# elem = driver.find_element_by_name("q")
-# elem.clear()
-# elem.send_keys("pycon")
-# elem.send_keys(Keys.RETURN)
-# assert "No results found." not in driver.page_source
-
 driver.execute_script("var myVar=setInterval(function(){myTimer()},1000); function myTimer() { window.scrollTo(0,document.body.scrollHeight);console.log(document.documentElement.scrollHeight)}")
-
-# driver.close()
+oldVal = driver.execute_script("return window.scrollY;")
+sleep(5)
+while oldVal != driver.execute_script("return window.scrollY;"):
+    oldVal = driver.execute_script("return window.scrollY;")
+    sleep(10)
+page_source = driver.page_source
+driver.quit()
+soup = BeautifulSoup(page_source, 'html.parser')
+all_tweets = [ tweet['data-tweet-id'] for tweet in soup.select("div.tweet")]
+driver2 = webdriver.Chrome()
+driver2.get("https://twitter.com/" + username + "/status/" + all_tweets[-1])
